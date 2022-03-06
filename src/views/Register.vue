ed08043628d354d02824f629e21d514f5702beaf
袁海涛 <yuanhaitao@olading.com>
2022-03-06 22:24:26 +0800

    目标:添加登录、注册两个组件到路由。完成

---
+++ src/views/Register.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    注册界面
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Register"
+}
+</script>
+
+<style scoped>
+
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,30 +1,18 @@
 module.exports = {
-    configureWebpack:{
-        devServer:{
-            port:8089,//端口号
-            open:true,
-            //Mock数据
-            before(app){
-
-            }
-        }
-    },
-
-    css: {
-      loaderOptions: {
-        stylus: {
-          'resolve url': true,
-          'import': [
-            './src/theme'
-          ]
-        }
-      }
-    },
-
-    pluginOptions: {
-      'cube-ui': {
-        postCompile: true,
-        theme: true
+  css: {
+    loaderOptions: {
+      stylus: {
+        'resolve url': true,
+        'import': [
+          './src/theme'
+        ]
       }
     }
+  },
+  pluginOptions: {
+    'cube-ui': {
+      postCompile: true,
+      theme: true
+    }
+  }
 }
